faf784e999090aba4032d1c97800b9315aace8a9
sagezeehaha <boncheturma.12@gmail.com>
2026-03-11 17:35:26 +0800

    Added some fixes in tutorial feature

--- components/projects/ProjectHeader.tsx
+++ components/projects/ProjectHeader.tsx
@@ -35,9 +35,9 @@ export default function ProjectHeader({ projectName, projectDescription }: Proje
 
     return (
         <div className="flex flex-col">
-            <h1 className="text-2xl md:text-[32px] font-black tracking-tightest leading-[0.8] uppercase max-w-7xl flex flex-col transition-all duration-300">
-                <span className="text-gray-900 dark:text-slate-50">
-                    {projectName} <span className="text-gray-200 dark:text-slate-800 font-light ml-2">/</span>
+            <h1 className="text-2xl md:text-[32px] font-black tracking-tightest leading-none uppercase max-w-7xl flex flex-col gap-1 md:gap-2 transition-all duration-300">
+                <span className="text-gray-900 dark:text-slate-50 flex items-center">
+                    {projectName} <span className="text-gray-200 dark:text-slate-800 font-light ml-3 opacity-50">/</span>
                 </span>
                 <span className="text-gray-900 dark:text-slate-50">{sectionName}</span>
             </h1>
